a2877cc93d9fb7eed5c968882ef61cea21714c79
Drashti Darji <drashti.darji@iqinfinite.in>
2025-05-08 18:01:47 +0530

    add fields in insert.php file

--- server/insert.php
+++ server/insert.php
@@ -1,8 +1,22 @@
 <?php 
 include 'db_connection.php';
 
+$name = $_POST["fullName"];
+$dob = $_POST["dob"];
+$email = $_POST["email"];
+$phone = $_POST["phone"];
+$gender = $_POST["gender"]; 
+$address = $_POST["address"];
+$pincode = $_POST["pincode"];
+$country = $_POST["country"];
+$state = $_POST["state"];
+$city = $_POST["city"];
+$documents = $_POST["document"];
+
 try {
-    if(!empty($name) && !empty($email) && !empty($pswd) && !empty($gender)){
+    if(!empty($fullName) && !empty($dob) && !empty($email) && !empty($phone) && !empty($gender) && 
+    !empty($address) && !empty($pincode) && !empty($country) && !empty($state) && !empty($city) && 
+    !empty($documents)){
         $stmt = $conn->prepare("INSERT INTO user(user_name,user_email,user_pswd,gender) 
                         VALUES (:user_name,:user_email,:user_pswd,:gender)");
         $stmt->execute([
