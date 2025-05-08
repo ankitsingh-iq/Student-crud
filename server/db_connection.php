b27e72fbc5db70e6e7835cd1405ca025c7e32f93
bansari parmar <bansari.parmar@iqinfinite.in>
2025-05-08 17:41:03 +0530

    added code in db connection file

--- server/db_connection.php
+++ server/db_connection.php
@@ -1,3 +1,20 @@
 <?php
+$servername = "localhost";
+$username = "root";
+$password = "Ami@2211!";
+$dbname = "student_management";
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//   $stmt = $conn->prepare("INSERT INTO MyGuests (firstname, lastname, email)
+//   VALUES (:firstname, :lastname, :email)");
+//   $stmt->bindParam(':firstname', $firstname);
+}
+catch(PODException $err){
+    echo $err.getMessage();
+}
+
 
 ?>
