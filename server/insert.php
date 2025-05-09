0cd9a96480912b019576aef75a1f991e94528852
Drashti Darji <drashti.darji@iqinfinite.in>
2025-05-09 18:57:53 +0530

    add insert query

--- server/insert.php
+++ server/insert.php
@@ -2,32 +2,44 @@
 include 'db_connection.php';
 
 $name = $_POST["fullName"];
-$dob = $_POST["dob"];
 $email = $_POST["email"];
 $phone = $_POST["phone"];
+$dob = $_POST["dob"];
 $gender = $_POST["gender"]; 
 $address = $_POST["address"];
 $pincode = $_POST["pincode"];
 $country = $_POST["country"];
 $state = $_POST["state"];
 $city = $_POST["city"];
-$documents = $_POST["document"];
+$documents = $_POST["documents"];
 
-try {
-    if(!empty($fullName) && !empty($dob) && !empty($email) && !empty($phone) && !empty($gender) && 
-    !empty($address) && !empty($pincode) && !empty($country) && !empty($state) && !empty($city) && 
-    !empty($documents)){
-        $stmt = $conn->prepare("INSERT INTO user(user_name,user_email,user_pswd,gender) 
-                        VALUES (:user_name,:user_email,:user_pswd,:gender)");
+$requiredFields = [$name, $email, $phone, $dob, $gender, $address, $pincode, $country, $state, $city, $documents];
+
+
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+    try {
+        $stmt = $conn->prepare("INSERT INTO students(full_name, dob, email, phone, ,gender, address, 
+                                pincode, country, state, city, documents) VALUES (:fName, :dob,
+                                :email, :phone, :gender, :address, :pin, :country, :state, :city, :doc)");
         $stmt->execute([
-            ':user_name' => $name,
-            ':user_email' => $email,
-            ':user_pswd' => $pswd,
-            ':gender' => $gender
+            ':fName' => $name,
+            ':dob' => $dob,
+            ':email' => $email,
+            ':phone' => $phone,
+            ':gender' => $gender,
+            ':address' => $address,
+            ':pin' => $pincode,
+            ':country' => $country,
+            ':state' => $state,
+            ':city' => $city,
+            ':doc' => $documents
         ]);
+
         echo '<script>alert("New record inserted successfully!!!")</script>';
+    } catch(PDOException $e) {
+        echo $sql . "<br>" . $e->getMessage();
     }
-} catch(PDOException $e) {
-    echo $sql . "<br>" . $e->getMessage();
+} else {
+    echo "error";
 }
 ?>
