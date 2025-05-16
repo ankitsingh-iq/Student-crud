e90ba17884298b9cab26fd5392b79653ad16ff45
Drashti Darji <drashti.darji@iqinfinite.in>
2025-05-16 14:52:25 +0530

    add function for choosing file for import

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+function formToggle(ID) {
+    var element = document.getElementById(ID);
+    if(element.style.display === "none") {
+        element.style.display = "block";
+    }else {
+        element.style.display = "none";
+    }
+}
 
 var selectObj = {
     "India" : { "Gujarat" : [ "Ahmedabad", "Gandhinagar", "Rajkot", "Surat"],
