b441950d0133d9fc260fb0be7410b809852891c2
Drashti Darji <drashti.darji@iqinfinite.in>
2025-05-16 16:55:56 +0530

    set alert message for successfull imported data using session

--- server/importData.php
+++ server/importData.php
@@ -1,18 +1,18 @@
 <?php
-
 session_start();
+
 include_once 'db_connection.php';
 
 if(isset($_POST['importSubmit'])) {
 
-    // Allowed mime types
+    // Allowed mime types to verify the uploaded file is a valid CSV format
     $csvMimes = array('text/x-comma-separated-values', 'text/comma-separated-values', 'application/octet-stream', 'application/vnd.ms-excel', 'application/x-csv', 'text/x-csv', 'text/csv', 'application/csv', 'application/excel', 'application/vnd.msexcel', 'text/plain');
 
     // Validate whether selected file is a CSV file
-    if(!empty($_FILES['file']['name']) && in_array($_FILES['file']['type'], $csvMimes)) {
+    if(!empty($_FILES['file']['name']) && in_array($_FILES['file']['type'], $csvMimes)) {   //in_array() - check file type is match csv file type
 
         // If the file is uploaded
-        if(is_uploaded_file($_FILES['file']['tmp_name'])) {
+        if(is_uploaded_file($_FILES['file']['tmp_name'])) {     //file was really uploaded via browser & HTTP POST
 
             // Open uploaded CSV file with read-only mode
             $csvFile = fopen($_FILES['file']['tmp_name'], 'r');
@@ -35,18 +35,17 @@ if(isset($_POST['importSubmit'])) {
                 $city    = $line[9];
                 $doc     = $line[10];
 
-                //Check whether member already exists in the database with the same email
-                $stmt = $conn->prepare("SELECT id FROM students WHERE full_name = :full_name");
-                $stmt->execute(['full_name' => $name]);
+                //Check whether member already exists in the database with the same name
+                $stmt = $conn->prepare("SELECT id FROM students WHERE email = :email");
+                $stmt->execute(['email' => $email]);
 
-                if($stmt->rowCount() > 0){
-                    // Update member data in the database
-                    $update = $conn->prepare("UPDATE students SET email = :email, phone = :phone,
+                if($stmt->rowCount() > 0) {
+                    $update = $conn->prepare("UPDATE students SET full_name = :full_name, phone = :phone,
                                             dob = :dob, gender = :gender, address = :address,
                                             pincode = :pin, country = :country, state = :state,
-                                            city = :city, documents = :doc WHERE full_name = :full_name");
+                                            city = :city, documents = :doc WHERE email = :email");
                     $update->execute([
-                                    'email' => $email,
+                                    'full_name' => $name,
                                     'phone' => $phone,
                                     'dob' => $dob,
                                     'gender' => $gender,
@@ -56,10 +55,14 @@ if(isset($_POST['importSubmit'])) {
                                     'state' => $state,
                                     'city' => $city,
                                     'doc' => $doc,
-                                    'full_name' => $name
+                                    'email' => $email
                     ]);
-                }else{
-                    // Insert member data in the database
+
+                    $_SESSION['status_type'] = 'warning';
+                    $_SESSION['status_msg'] = "This Email is already Exist!!! Please Update the data!!!";
+                    header("Location: ../index.php");
+                    exit();
+                }else {
                     $insert = $conn->prepare("INSERT INTO students(full_name, dob, email, phone, gender,
                                 address,pincode, country, state, city, documents) VALUES
                                 (:fName, :dob, :email, :phone, :gender, :address, :pin, :country,
@@ -83,32 +86,26 @@ if(isset($_POST['importSubmit'])) {
 
             // Close opened CSV file
             fclose($csvFile);
-            // echo "<script>
-            //     alert('Students data has been imported successfully!!!')
-            //     </script>";
-            //$qstring = '?id=succ';
+
+            $_SESSION['status_type'] = 'success';
             $_SESSION['status_msg'] = "Students data has been imported successfully!!!";
             header("Location: ../index.php");
             exit();
+
         }else {
+
+            $_SESSION['status_type'] = 'danger';
             $_SESSION['status_msg'] = "Error uploading the file.";
             header("Location: ../index.php");
             exit();
-            //$qstring = '?id=err';
-            // echo "<script>
-            //     alert('Some problem occurred, please try again!!!')
-            //     </script>";
         }
+
     }else {
+
+        $_SESSION['status_type'] = 'warning';
         $_SESSION['status_msg'] = "Invalid file type. Please upload a CSV file.";
         header("Location: ../index.php");
         exit();
-        //$qstring = '?id=invalid_file';
-        // echo "<script>
-        //         alert('Please upload a valid CSV file!!!')
-        //         </script>";
     }
 }
-
-//header("Location: ../index.php");
 ?>
