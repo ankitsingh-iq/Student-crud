5eed38f2d1d459849e87073c98e717eda77d2e59
bansari parmar <bansari.parmar@iqinfinite.in>
2025-05-16 10:30:34 +0530

    added dynamic dropdown

--- js/script.js
+++ js/script.js
@@ -1,94 +1,72 @@
 $(document).ready();
+var selectObj = {
+    "India" : { "Gujarat" : [ "Ahmedabad", "Gandhinagar", "Rajkot", "Surat"],
+        "Goa" : [ "Panaji", "Margao", "Vasco da Gama", "Ponda"],
+        "Kerala" : [ "Thiruvananthapuram", "Kochi", "Kollam", "Kozhikode"],
+        "Punjab" : [ "Amritsar", "Chandigarh", "jalandhar", "Patiala"],
+    },
+    "Australia" : { "Victoria" : [ "Geelong", "Ballarat", "Bendigo"],
+        "Western Australia" : [ "Perth", "Bunbury", "Geraldton"],
+        "South Australia" : [ "Adelaide", "Mount Gambier"],
+    },
+    "Brazil" : { "Sao Paulo" : [ "Campinas", "Osasco"],
+        "Distrito Federal" : [ "Milpa Alta", "Iztacalco"],
+    },
+    "Canada" : { "Ontario" : [ "Toronto", "Ottawa"],
+        "Alberta" : [ "Calgary", "Edmonton"],
+        "Manitoba" : [ "Brandon", "Winnipeg"],
+    },
+    "Denmark" : { "Zeeland" : [ "Veere", "Goes"],
+        "Funen" : [ "Odense", "Nyborg"],
+        "Greenland" : [ "Nuuk", "Sisimiut"],
+    }
+}
 
-// // loaddata()
-$(document).ready(function ()
-    {
-      $("#myform").validate({
-        rules: {
-          full_name:{
-              required: true
-          },
-          dob:{
-              required: true
-          },
-          email: {
-              required: true,
-              email: true
-          },
-          phone: {
-              required: true,
-              minlength: 10,
-              maxlength: 10
-          },
-          gender: {
-              required: true
-          },
-          address: {
-              required: true
-          },
-          pincode: {
-              required: true,
-              maxlength:6
-          },
-          country: {
-              required: true
-          },
-          state: {
-              required: true
-          },
-          city: {
-              required: true
-          },
-          documents:
-          {
-              fileRequired: true
-          }
-        },
-        messages: {
-            full_name: {
-                required: "Name is required",
-            },
-            dob: {
-                required: "Date of Birth is required"
-            },
-            email: {
-                required: "Email is required",
-                email: "Enter a valid email address"
-            },
-            phone: {
-                required: "Phone number is required",
-                minlength: "Phone number must be 10 digits",
-                maxlength: "Phone number must be 10 digits"
-            },
-            gender: {
-                required: "Please select gender"
-            },
-            address: {
-                required: "Address is required"
-            },
-            pincode: {
-                required: "Pincode is required",
-                maxlength:"Pincode have maximum 6 length"
-            },
-            country: {
-                required: "Country is required"
-            },
-            state: {
-                required: "State is required"
-            },
-            city: {
-                required: "City is required"
-            },
-            documents: {
-                required: "Please upload documents"
-            }
-        },
-        errorElement: "div",
-      errorClass: "text-danger"
+function selectFun() {
+    var countrySel = document.getElementById("country");
+    var stateSel = document.getElementById("state");
+    var citySel = document.getElementById("city");
 
-    })
+    for(var c in selectObj) {
+        countrySel.options[countrySel.options.length] = new Option(c, c);
+    }
+    countrySel.onchange = function(){
+        stateSel.length = 1;    //remove all options bar first
+        citySel.length = 1;     //remove all options bar first
+
+        if(this.selectedIndex < 1)  return;     //done
+
+        for(var s in selectObj[this.value]){
+            stateSel.options[stateSel.options.length] = new Option(s, s);
+        }
+    }
+    countrySel.onchange();      // reset in case page is reloaded
+    stateSel.onchange = function() {
+        citySel.length = 1;
+
+        if(this.selectedIndex < 1)  return;
+
+        var cty = selectObj[countrySel.value][this.value];
+        for(var i = 0; i < cty.length; i++){
+            citySel.options[citySel.options.length] = new Option(cty[i], cty[i]);
+        }
+    }
+}
+Swal.fire(
+    'Error!',
+    data.message || 'Failed to insert the student.',
+    'error'
+);
+Swal.fire({
+    title: 'Are you sure?',
+    text: "This student will be deleted permanently!",
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#d33',
+    cancelButtonColor: '#3085d6',
+    confirmButtonText: 'Yes, delete it!'
+}).then((result) => {
+    if (result.isConfirmed) {
+// to do if confirmation is  yes
+}
 })
-$.validator.addMethod("fileRequired", function (value, element)
-{
-    return element.files.length > 0;
-}, "Please upload at least one file.");
